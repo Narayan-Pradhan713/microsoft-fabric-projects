ba776f20064d0d0c1be0a0945c8e21d7f3c2b6d9
NARAYAN PRADHAN <narayanpradhan713@gmail.com>
2026-09-07 12:02:28 +0530

    Delete sales-returns-analytics/sql/02_create_control_tables.sql.sql

--- sales-returns-analytics/sql/02_create_control_tables.sql.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE dbo.WatermarkControl(SourceTable VARCHAR(100) PRIMARY KEY,WatermarkColumn VARCHAR(100),LastWatermark DATETIME2,TargetTable VARCHAR(100),LakehouseTarget VARCHAR(100),WarehouseTarget VARCHAR(100),LoadType VARCHAR(50),IsActive BIT);
-CREATE TABLE dbo.MetadataControl(EntityName VARCHAR(100) PRIMARY KEY,SourceType VARCHAR(50),SourceObject VARCHAR(200),TargetTable VARCHAR(100),WatermarkColumn VARCHAR(100),BusinessKey VARCHAR(100),LoadType VARCHAR(50),TargetType VARCHAR(50),IsActive BIT);
-
